0d772eb272d7058bb0559901fc090d5224e02209
Bruno Olympio <b.olympio@reply.com>
2022-02-23 11:14:37 -0300

    Seção 7: Operadores

---
+++ ConversorTemperaturas/ConversorTemperaturas/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConversorTemperaturas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double c, f, k;
+            Console.WriteLine("### CONVERSOR DE TEMPERATURA ###");
+            Console.Write("Insira a temperatura em Celcius: ");
+            c = double.Parse(Console.ReadLine());
+            Console.WriteLine("----------------------------------");
+
+            f = (c * 9 / 5) + 32;
+            k = (c + 273.15);
+
+            Console.WriteLine(c + " graus celcius = " + f + " graus fahrenheit");
+            Console.WriteLine(c + " graus celcius = " + k + " kelvin");
+            Console.WriteLine("----------------------------------");
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ Operadores/Operadores/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace Operadores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Operadores Matemáticos
+            //int num1 = 10;
+            //int num2 = 2;
+            //int resultado = num1 + num2;
+            //Console.WriteLine(num1 + num2);
+            //Console.WriteLine(num1 - num2);
+            //Console.WriteLine(num1 * num2);
+            //Console.WriteLine(num1 / num2);
+            //Console.WriteLine(num1 % num2);
+            #endregion
+
+            #region Precedência de Operadores
+            //double num1 = 100;
+            //double num2 = 10;
+            //double num3 = 5;
+
+            //double res = (num1 + num2) * (num3 / 3);
+            #endregion
+
+            #region Operadores de Incremento e Decremento
+            //int num1 = 10;
+            //num1++;
+            //Console.WriteLine(num1++);
+            #endregion
+
+            #region Operadores de Concatenação
+
+            //string nome = "Bruno";
+            //string sobreNome = "Ferreira";
+            //string nomeCompleto = nome + sobreNome;
+
+            #endregion
+
+            #region Operadores de Atribuição
+
+            //int num1 = 10;
+            //num1 += 20;
+            //num1 -= 2;
+            //num1 *= 3;
+            //num1 /= 2;
+            //num1 %= 2;
+
+            //string nome = "Bruno";
+            //nome += " Ferreira";
+
+            #endregion
+
+            #region Operadores de Igualdade
+
+            //bool res = 100 == (50 * 2);
+            //bool res2 = (200 / 2) != (100 + 100);
+
+            #endregion
+
+            #region Operadores Relacionais
+
+            //bool res = 100 < 50 + 65;
+            //bool res1 = 100 > 50 + 65;
+            //bool res2 = 100 <= 50 + 65;
+            //bool res3 = 100 >= 50 + 65;
+
+            //Console.WriteLine(res);
+            //Console.WriteLine(res1);
+            //Console.WriteLine(res2);
+            //Console.WriteLine(res3);
+
+            #endregion
+
+            #region Operadores Lógicos
+
+            //bool res1 = 100 > 50;
+            //bool res2 = 50 == 50;
+
+            //bool final = res1 && res2;
+            //bool final2 = res1 || res2;
+
+            #endregion
+
+
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
