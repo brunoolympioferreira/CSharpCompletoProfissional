612a16113a7fe4870d1e2f80cc97919b65c237fa
Bruno Olympio <b.olympio@reply.com>
2022-02-25 13:48:56 -0300

    Seção 8: Coleções do tipo Array

---
+++ Colecao/Colecao/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Colecao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Array Unidimensional (Vetores)
+            //int[] numeros = new int[5];
+
+            //numeros[0] = 10;
+            //numeros[1] = 20;
+            //numeros[2] = 30;
+            //numeros[3] = 40;
+            //numeros[4] = 50;
+
+            //numeros[2] = 55;
+
+            //string[] nomes = { "Bruno", "Vinicius", "Vivian" };
+            //Console.WriteLine(numeros[2]);
+            //Console.WriteLine(nomes[2]);
+            #endregion
+
+            #region Array Bidimensional (Matriz)
+            //int[,] numeros = new int[2, 3];
+
+            //numeros[0, 0] = 10;
+            //numeros[0, 1] = 20;
+            //numeros[0, 2] = 30;
+
+            //numeros[1, 0] = 40;
+            //numeros[1, 1] = 50;
+            //numeros[1, 2] = 60;
+
+            //Console.Write("[" + numeros[0, 0] + "]");
+            //Console.Write("[" + numeros[0, 1] + "]");
+            //Console.Write("[" + numeros[0, 2] + "]");
+
+            //Console.WriteLine();
+
+            //Console.Write("[" + numeros[1, 0] + "]");
+            //Console.Write("[" + numeros[1, 1] + "]");
+            //Console.Write("[" + numeros[1, 2] + "]");
+
+            //Console.WriteLine();
+
+            //string[,] nomes =
+            //{
+            //    {"Bruno","Vivian","Vinicius" },
+            //    {"Raphaela","Rodolfo","Dandara" }
+            //};
+
+            //Console.WriteLine(nomes[0,1]);
+            #endregion
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
