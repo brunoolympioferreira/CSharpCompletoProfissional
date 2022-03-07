9b9ffcaec54f87910d74afa8a49fcecf89a7f6a5
Bruno Olympio <b.olympio@reply.com>
2022-03-07 14:38:05 -0300

    Seção 11: Estruturas de Repetição

---
+++ CSharpBasico/9_Ciclos/9_Ciclos/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace _9_Ciclos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region While
+            //int valor = 0;
+            //while (valor < 10)
+            //{
+            //    Console.WriteLine("O valor é " + valor);
+            //    valor++;
+            //}
+            #endregion
+
+            #region do-While
+
+            //int valor = 15;
+            //do
+            //{
+            //    Console.WriteLine("O valor é: " + valor);
+            //    valor++;
+            //} while (valor < 10);
+
+
+            #endregion
+
+            #region For
+
+            //for (int i = 0, j = 10; i <10; i++, j--)
+            //{
+            //    Console.WriteLine("I = " + i + " | J = " + j);
+            //}
+
+            #endregion
+
+            #region ForEach
+
+            //string[] nomes = { "Gabriel", "Danny", "Arthur", "João" };
+
+            //foreach (string nome in nomes)
+            //{
+            //    Console.WriteLine("Nome: " + nome);
+            //}
+
+            #endregion
+
+            Console.ReadKey();
+        }
+    }
+}
