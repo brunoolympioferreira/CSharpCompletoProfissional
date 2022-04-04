36d5b509b83fa420912fd5c0c5ff09109c27a302
Bruno Olympio <b.olympio@reply.com>
2022-04-04 10:22:42 -0300

    Seção 13: Recursividade

---
+++ Recursividade/MetodoMain/MetodoMain/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetodoMain
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string senha = "abc123";
+
+            if (senha != args[1])
+            {
+                Console.WriteLine("Acesso Negado!");
+            }
+            else
+            {
+                Console.WriteLine("Olá " + args[0]);
+            }
+        }
+    }
+}
---
+++ Recursividade/Recursividade/Recursividade/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursividade
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Recursivo recursivo = new Recursivo();
+            recursivo.ExecutarRecursivo("Ola mundo", 5);
+
+
+            Console.ReadKey();
+        }
+    }
+}
